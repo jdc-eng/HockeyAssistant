f3f49fed4c6d9a5941814e8b5e241800f5d76135
jack crespo <jdc344@cornell.edu>
2022-02-06 00:16:36 -0500

    Changes: -More robust at handling the roster page- now selects the list view from the roster webpage before it checks for it; inspired/necessitated by RPI and Minn. State roster sites. -Added built version for using edge browser; in /Build-Edge. To get working app on device, just download this folder. It includes the 'Retrieval UI.exe' and the msedgewebdriver needed to run the app. -Directory clean up of unnecessary files; deleted the extra build and dist folder, csv files, etc.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='HockeyAssistant',
+    version='',
+    packages=[''],
+    url='https://github.com/jdc-eng/HockeyAssistant.git',
+    license='',
+    author='Jack Crespo',
+    author_email='jdc344@cornell.edu',
+    description=''
+)
+
+#### this is working thing  re.sub(r'\s+', ' ', string=re.sub(r'[^a-zA-Z0-9\s]', '', string
+
+##this is current line 69  re.sub(r'[^a-zA-Z0-9\s]', '', re.sub(r'\s+', '', string=player.contents[3].contents[4].text).strip())
